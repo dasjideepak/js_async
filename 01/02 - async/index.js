6ea4533bf53c537e4d4037057dbd233480c51179
dasjideepak <dasjideepak@gmail.com>
2020-03-30 19:08:49 +0530

    github project complete

--- 01/02 - async/index.js
+++ 01/02 - async/index.js
@@ -8,48 +8,80 @@ function testMe() {
   setTimeout(sayHowdy, 0);
   console.log("Partnah");
 }
-// After thinking it through, uncomment the following line to check your guess!
-// testMe(); // what order should these log out? Howdy or Partnah first?
+
+testMe(); // "Partnah" first then "Howdy".
 
 /* CHALLENGE 2 */
 
 function delayedGreet() {
-  // ADD CODE HERE
+  setTimeout(() => console.log("Welcome"), 3000);  
 }
-// Uncomment the following line to check your work!
-// delayedGreet(); // should log (after 3 seconds): welcome
+
+delayedGreet();
 
 /* CHALLENGE 3 */
 
 function helloGoodbye() {
-  // ADD CODE HERE
+  console.log("Hello");
+  setTimeout(() => console.log("Good Bye"), 2000);  
 }
-// Uncomment the following line to check your work!
-// helloGoodbye(); // should log: hello // should also log (after 3 seconds): good bye
+
+helloGoodbye();
 
 /* CHALLENGE 4 */
 
+var myVar;
+function brokenRecord() {
+  myVar = setInterval(() => {
+    console.log("Hello")
+  }, 1000);
+}
+
+function stop(event) {
+    console.log(event.target);
+    clearInterval(myVar);
+};
+
+document.addEventListner('keyup', stop) 
+  
+var every;
 function brokenRecord() {
   // ADD CODE HERE
+ every=setInterval(() => {
+  console.log("hi again")
+}, 1000);
+
 }
-// Uncomment the following line to check your work!
-// brokenRecord(); // should log (every second): hi again
+
+function clear(event){
+  console.log(event.target)
+  clearInterval(every)
+}
+
+document.addEventListener("keydown",clear);
+
 
 /* CHALLENGE 5 */
 
+var myVar;
+
 function limitedRepeat() {
-  // ADD CODE HERE
+  myVar = setInterval(() => {
+    console.log("Hello")
+  }, 1000);
 }
-// Uncomment the following line to check your work!
-// limitedRepeat(); // should log (every second, for 5 seconds): hi for now
+
+clearInterval(myVar);
+
+limitedRepeat(); // should log (every second, for 5 seconds): hi for now
 
 /* CHALLENGE 6 */
 
 function everyXsecsForYsecs() {
   // ADD CODE HERE
 }
-// Uncomment the following lines to check your work!
-// function theEnd() {
-//   console.log('This is the end!');
-// }
-// everyXsecsForYsecs(theEnd, 2, 20); // should invoke theEnd function every 2 seconds, for 20 seconds): This is the end!
+
+function theEnd() {
+  console.log('This is the end!');
+}
+everyXsecsForYsecs(theEnd, 2, 20); 
